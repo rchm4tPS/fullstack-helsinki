39984afec067eeb0322bcf48a225e7a3ddb8dfb9
rchm4tPS <rachmatpurwa393@gmail.com>
2025-11-02 21:27:17 +0700

    Do the exercise 1.3

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -4,24 +4,27 @@ import Total from "./components/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
-  const contents = [
-    {part: part1, exercise: exercises1},
-    {part: part2, exercise: exercises2},
-    {part: part3, exercise: exercises3}
-  ]
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const contents = [part1, part2, part3]
 
   return (
     <div>
       <Header course={course} />
       <Content contents={contents} />
-      <Total total={exercises1+exercises2+exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
--- part1/part1/src/components/Content.jsx
+++ part1/part1/src/components/Content.jsx
@@ -7,8 +7,8 @@ export default function Content({contents}) {
                 return (
                     <Part 
                         key={index}
-                        part={content.part} 
-                        exercise={content.exercise} 
+                        part={content.name} 
+                        exercise={content.exercises} 
                     />
                 )
             })}
