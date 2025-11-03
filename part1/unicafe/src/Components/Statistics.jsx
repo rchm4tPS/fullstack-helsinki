c459fb82b24e0a9ac7482fe565fb50af70f80f0a
rchm4tPS <rachmatpurwa393@gmail.com>
2025-11-03 11:39:07 +0700

    - finish exercise 1.7 - add CSS to get rid of p tag margin

--- part1/unicafe/src/Components/Statistics.jsx
+++ part1/unicafe/src/Components/Statistics.jsx
@@ -2,12 +2,25 @@ export default function Statistics({
     good, neutral, bad
 })
 {
+    const GOOD_SCORE = 1
+    const NEUTRAL_SCORE = 0
+    const BAD_SCORE = -1
+
+    const total = good + neutral + bad
+    const averageScore = ((good * GOOD_SCORE)
+                       + (neutral * NEUTRAL_SCORE)
+                       + (bad * BAD_SCORE)) / total
+    const positivePercentage = good / total * 100
+
     return (
         <>
             <h2>statistics</h2>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>average {averageScore || 0}</p>
+            <p>positive {positivePercentage || 0}%</p>
         </>
     )
 }
