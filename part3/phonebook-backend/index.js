b10edf89248cdd5065c75f69fcbb8218cb5ae8ba
rchm4tPS <rachmatpurwa393@gmail.com>
2025-11-08 00:52:33 +0700

    Finishing exercise 3.15 up until 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 
-const PORT = process.env.PORT || 3001
+import PhoneBook from './models/phonebook.js'
+
+const PORT = process.env.PORT
 
 const app = express()
-app.use(express.json())
+
 app.use(express.static('dist'))
+app.use(express.json())
 
 // Middleware: Morgan
 // if using tiny configuration (for exercise 3.7)
@@ -32,108 +35,128 @@ app.use(
     })
 )
 
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 // Routers
 
 // GET group
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (req, res, next) => {
+    PhoneBook.find({}).then(fetchedEntries => {
+        res.json(fetchedEntries)
+    })
+    .catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     const personId = req.params.id
-    const person = persons.find(person => person.id === personId)
 
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).json({
-            error: `Person with ID ${personId} could not be found!`
-        }).end()
-    }
+    PhoneBook.findById(personId)
+        .then(founded => {
+            if (!founded) {
+                return next({ 
+                    status: 404,
+                    message: `Person with ID ${personId} not found` 
+                })
+            }
+            res.json(founded)
+        })
+        .catch(err => next(err))
 })
 
-app.get('/info', (req, res) => {
-    res.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date().toString()}</p>    
-    `).end()
+app.get('/info', (req, res, next) => {
+    PhoneBook.find({})
+        .then(fetched => {
+            res.send(`
+                <p>Phonebook has info for ${fetched.length} people</p>
+                <p>${new Date().toString()}</p>    
+            `)
+        })
+        .catch(err => next(err))
 })
 
 // POST group
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const newName = req.body.name
-    const newNumber = req.body.number
+    const newNumber = req.body.phone
 
     if (newName && newNumber) {
-        const isNameAlreadyExist = persons.find(person => person.name === newName)
+        const newDataToAdd = new PhoneBook({
+            name: newName,
+            phone: newNumber
+        })
 
-        if (!isNameAlreadyExist) {
-            const newDataToAdd = {
-                id: String(Math.ceil(Math.random() * Math.pow(10,10))),
-                name: newName,
-                number: newNumber
-            }
+        newDataToAdd.save()
+            .then(savedData => {
+                res.json(savedData)
+            })
+            .catch(err => next(err))
 
-            persons = persons.concat(newDataToAdd)
-
-            res.json(newDataToAdd)
-        }
-        else {
-            return res.status(409).json({
-                error: "Error: The data is already exist in the repository and could not be duplicated!"
-            }).end()
-        }
     } else {
-        res.status(400).json({
-            error: "Invalid body request! Either name or number are not passed."
-        }).end()
-    }
-}) 
-
-// DELETE group
-app.delete('/api/persons/:id', (req, res) => {
-    const personId = req.params.id
-    // Return deleted data object back to the front-end
-    // to update its state, so the updated list can be re-rendered
-    const deletedPerson = persons.find(person => person.id === personId)
-    
-    if (deletedPerson) {
-        persons = persons.filter(person => person.id !== personId)
-        
-        return res.json(deletedPerson).status(204)
-    }
-    else {
-        res.status(404).json({
-            error: `Could not delete person with ID ${personId} which does not exist!`
-        }).end()
+        next({
+            status: 400,
+            message: "Invalid body request! Either name or number are not passed."
+        })
     }
 })
 
+// PUT group
+app.put('/api/persons/:id', (req, res, next) => {
+    const { name, phone } = req.body
+
+    PhoneBook.findByIdAndUpdate(
+        req.params.id,
+        { name, phone },
+        { new: true, runValidators: true, context: 'query' }
+    )
+    .then(updated => {
+        if (!updated) return next({
+            status: 404,
+            message: "Person not found!"
+        })
+
+        res.json(updated)
+    })
+    .catch(err => next(err))
+})
+
+// DELETE group
+app.delete('/api/persons/:id', (req, res, next) => {
+    const personId = req.params.id
+    // Return deleted data object back to the front-end
+    // to update its state, so the updated list can be re-rendered
+
+    PhoneBook.findByIdAndDelete(personId)
+        .then(deletedData => {
+            if (!deletedData) {
+                return next({
+                    status: 404,
+                    message: `Could not delete person with ID ${personId} which does not exist!`
+                })
+            }
+            res.status(200).json(deletedData)
+        })
+        .catch(err => next(err))
+})
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.name, err.message)
+
+    if (err.name === 'CastError') {
+        return res.status(400).json({ error: 'malformatted id' })
+    }
+
+    if (err.name === 'ValidationError') {
+        return res.status(400).json({ error: err.message })
+    }
+
+    // handle custom thrown errors
+    return res.status(err.status || 500).json({
+        error: err.message || 'Something went wrong on the server!'
+    })
+}
+
+app.use(errorHandler)
+
 // Listening
 app.listen(PORT, () => {
+    console.log(`Connecting to ${process.env.MONGODB_DB_CLUSTER_SHARDS}`)
     console.log(`Server running on http://localhost:${PORT}`)
 })
