cce9ebb5857a33fe77c198bc1ec509f4055235f0
rchm4tPS <rachmatpurwa393@gmail.com>
2025-11-08 19:06:05 +0700

    Add morgan logger

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,15 +1,37 @@
 import config from './utils/config.js'
 import express from 'express'
 import mongoose from 'mongoose'
-
-import bloglistRouter from './controllers/bloglist.js'
-// import Blog from './models/bloglist.js'
+import morgan from 'morgan'
 import logger from './utils/logger.js'
 import middleware from './utils/middleware.js'
+import bloglistRouter from './controllers/bloglist.js'
+import Blog from './models/bloglist.js'
 
 const PORT = config.PORT
 
 const app = express()
+
+// Middleware: Morgan
+morgan.token('body', (req) => {
+  if (req.body && Object.keys(req.body).length > 0) {
+    return JSON.stringify(req.body)
+  }
+
+  return '-'
+})
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms', '-',
+      tokens.body(req, res)
+    ].join(' ')
+  })
+)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -19,7 +41,19 @@ mongoose.connect(config.MONGODB_URI)
     logger.error(err)
   })
 
+app.use(express.static('dist'))
 app.use(express.json())
+
+app.get('/info', (req, res, next) => {
+  Blog.find({})
+    .then(fetched => {
+      res.send(`
+        <p>Blog database has info for ${fetched.length} blogs.</p>
+        <p>${new Date().toString()}</p>
+      `)
+    })
+    .catch(err => next(err))
+})
 
 app.use('/api/blogs', bloglistRouter)
 
