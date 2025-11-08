1a1aea4c84ded2c1015c45836c2023998276ce0f
rchm4tPS <rachmatpurwa393@gmail.com>
2025-11-08 09:29:36 +0700

    Finishing exercise 3.19 - 3.21

--- part3/phonebook-backend/models/phonebook.js
+++ part3/phonebook-backend/models/phonebook.js
@@ -26,8 +26,20 @@ mongoose.connect(url)
     })
 
 const phoneBookSchema = new mongoose.Schema({
-    name: String,
-    phone: String
+    name: {
+        type: String,
+        minLength: 3,
+        required: [true, "Why don't enter your name?"]
+    },
+    phone: {
+        type: String,
+        trim: true,
+        validate: {
+            validator: (p) => /^\d{2,3}-\d{5,}$/.test(p),
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required!']
+    }
 })
 
 phoneBookSchema.set('toJSON', {
